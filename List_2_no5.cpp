6ac3d985d2fc625011c510af4e4f772968a7ac13
Vasu Sapariya <141891510+vasusapa@users.noreply.github.com>
2023-08-24 08:35:25 +0530

    List_2_no5.cpp

---
+++ List_2_no5.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+	int a=1234;
+	float b=24.121234;
+	cout<<"220130318016"<<endl;
+	cout<<"Vasu sapariya"<<endl;
+	cout<<"value of a is :: "<<setfill('@')<<setw(10)<<a<<endl;
+	cout<<"value of b is :: "<<setprecision(3)<<b<<endl;
+	return 0;
+	
+}
