9acf2e5a057797a9e21d46a41e82e040b5546bdb
Vasu Sapariya <141891510+vasusapa@users.noreply.github.com>
2023-08-24 08:43:25 +0530

    List_2_no4.cpp

---
+++ List_2_no4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>    
+#include <iomanip>      
+using namespace std;  
+int main () 
+{
+  cout<<"220130318016"<<endl;
+  cout<<"Vasu sapariya"<<endl;
+  cout << setw(5)<<setfill('0');  
+  cout << 24 << endl;  
+  return 0;  
+}   
