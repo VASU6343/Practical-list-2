8597feebda1c28114b558a952ccc56133b095906
Vasu Sapariya <141891510+vasusapa@users.noreply.github.com>
2023-08-24 07:58:58 +0530

    Pro1no1.cpp

--- Pro1no1.cpp
+++ Pro1no1.cpp
@@ -4,8 +4,8 @@ int x=100;
 int main()
 {
 	int x=200;
-  cout<<"220130318016"<<endl;
-  cout<<"Vasu Sapariya";
+  	cout<<"220130318016"<<endl;
+  	cout<<"Vasu Sapariya";
 	cout<<"value of (local variable) x is ::"<<x<<endl;
 	cout<<"value of (global variable) x is ::"<<::x<<endl;
 	return 0;
