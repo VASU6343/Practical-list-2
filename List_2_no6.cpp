0e2220d65006e926a4f0abc37901a23d99b13a79
Vasu Sapariya <141891510+vasusapa@users.noreply.github.com>
2023-08-24 08:55:26 +0530

    List_2_no6.cpp

---
+++ List_2_no6.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<iomanip>
+using namespace std; 
+int main()
+{
+	int a =2594;
+	cout<<"220130318016"<<endl;
+	cout<<"Vasu sapariya"<<endl;
+	cout<<"The value of A=>"<<setw(10)<<setfill('*')<<a;
+	return 0;
+}
