31ee6539e2987b87194745acf17730677304759e
Vasu Sapariya <141891510+vasusapa@users.noreply.github.com>
2023-08-24 08:20:38 +0530

    List_2_no3.cpp

---
+++ List_2_no3.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+class temp
+{
+	public:
+		void display();
+};
+void temp::display()
+{
+	cout<<"220130318016"<<endl;
+	cout<<"Vasu sapariya"<<endl;
+	cout<<"Welcome to DSU"<<endl;
+}
+int main()
+{
+	temp t;
+	t.display();
+	return 0;
+}
