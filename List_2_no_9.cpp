c4d3c0490c10a915362ff951f745ba5f62a36364
Vasu Sapariya <141891510+vasusapa@users.noreply.github.com>
2023-08-26 20:15:13 +0530

    List_2_no_9.cpp

---
+++ List_2_no_9.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+    int a,b;
+    cout<<"220130318016"<<endl;
+	  cout<<"Vasu Sapariya "<<endl;
+    cout << "Enter the first number: ";
+    cin>>a;
+    cout << "Enter the second number: ";
+    cin>>b;
+    int sum =a+b;
+    cout << "Sum of " << a << " and " << b << " is: " << sum << endl;
+    return 0;
+}
