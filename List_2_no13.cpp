f4485bea593e398e92af227841118c37c4666188
Vasu Sapariya <141891510+vasusapa@users.noreply.github.com>
2023-08-26 21:27:03 +0530

    List_2_no13.cpp

---
+++ List_2_no13.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+class temp 
+{
+    public:
+        void display();
+};
+void temp::display() 
+{
+    cout<<"220130318016<<endl;
+	  cout<<"Vasu Sapariya "<<endl;
+    cout<"MY NAME IS VASU" <<endl;
+}
+int main() 
+{
+    temp t;
+    t.display();
+    return 0;
+}
