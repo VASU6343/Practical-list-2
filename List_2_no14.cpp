7b41865f50bc36d2dc6098015bbe075fb3c33c0b
Vasu Sapariya <141891510+Vasu1309@users.noreply.github.com>
2023-09-15 08:30:42 +0530

    List_2_no14.cpp

---
+++ List_2_no14.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+inline int cube(int r)
+{
+	return r*r*r;
+}
+int main()
+{
+	int r;
+	cout<<"220130318016"<<endl;
+	cout<<"Vasu Sapariya"<<endl;
+	cout<<"enter the number::";
+	cin>>r;
+	cout<<"cube is ::"<<cube(r);
+	return 0;
+	
+}
