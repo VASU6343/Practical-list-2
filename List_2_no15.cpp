bf0ca36ed7182aafd29458841d16b8c7bde95464
Vasu Sapariya <141891510+Vasu1309@users.noreply.github.com>
2023-09-15 08:31:41 +0530

    List_2_no15.cpp

---
+++ List_2_no15.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+inline int square(int no1)
+{
+	return no1*no1;
+}
+int main()
+{
+	int no1;
+	cout<<"220130318016"<<endl;
+	cout<<"Vasu Sapariya"<<endl;
+	cout<<"Enter the number::";
+	cin>>no1;
+	cout<<"Square number is::"<<square(no1);
+	
+	return 0;
+}
